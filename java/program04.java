ebeaed18e4cc4b4d67150186f3aaef6ca655d066
Pranav <pranavn2034@gmail.com>
2026-06-01 13:23:20 +0530

    display factor

---
+++ java/program04.java
@@ -0,0 +1,11 @@
+public class program04 {
+    public static void main(String[] args) {
+        int number = 28;
+        System.out.println("Factors of " + number + ":");
+        for (int i = 1; i <= number; i++) {
+            if (number % i == 0) {
+                System.out.println(i);
+            }
+        }
+    }
+}
